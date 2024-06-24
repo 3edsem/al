4bdb8ef91dbadd2a37b853d1a39f7ad2b75a0948
3edsem <160483215+3edsem@users.noreply.github.com>
2024-06-24 22:25:36 +0530

    Update p1.c

--- p1.c
+++ p1.c
@@ -1,98 +1,75 @@
-    #include <stdio.h>  
-    #include <stdlib.h>  
-      
-    #define MAX_EDGES 1000  
-      
-    typedef struct Edge {  
-        int src, dest, weight;  
-    } Edge;  
-      
-    typedef struct Graph {  
-        int V, E;  
-        Edge edges[MAX_EDGES];  
-    } Graph;  
-      
-    typedef struct Subset {  
-        int parent, rank;  
-    } Subset;  
-      
-    Graph* createGraph(int V, int E) {  
-        Graph* graph = (Graph*) malloc(sizeof(Graph));  
-        graph->V = V;  
-        graph->E = E;  
-        return graph;  
-    }  
-      
-    int find(Subset subsets[], int i) {  
-        if (subsets[i].parent != i) {  
-            subsets[i].parent = find(subsets, subsets[i].parent);  
-        }  
-        return subsets[i].parent;  
-    }  
-      
-    void Union(Subset subsets[], int x, int y) {  
-        int xroot = find(subsets, x);  
-        int yroot = find(subsets, y);  
-      
-        if (subsets[xroot].rank < subsets[yroot].rank) {  
-            subsets[xroot].parent = yroot;  
-        } else if (subsets[xroot].rank > subsets[yroot].rank) {  
-            subsets[yroot].parent = xroot;  
-        } else {  
-            subsets[yroot].parent = xroot;  
-            subsets[xroot].rank++;  
-        }  
-    }  
-      
-    int compare(const void* a, const void* b) {  
-        Edge* a_edge = (Edge*) a;  
-        Edge* b_edge = (Edge*) b;  
-        return a_edge->weight - b_edge->weight;  
-    }  
-      
-    void kruskalMST(Graph* graph) {  
-        Edge mst[graph->V];  
-        int e = 0, i = 0;  
-      
-        qsort(graph->edges, graph->E, sizeof(Edge), compare);  
-      
-        Subset* subsets = (Subset*) malloc(graph->V * sizeof(Subset));  
-        for (int v = 0; v < graph->V; ++v) {  
-            subsets[v].parent = v;  
-            subsets[v].rank = 0;  
-        }  
-      
-        while (e < graph->V - 1 && i < graph->E) {  
-            Edge next_edge = graph->edges[i++];  
-      
-            int x = find(subsets, next_edge.src);  
-            int y = find(subsets, next_edge.dest);  
-      
-            if (x != y) {  
-                mst[e++] = next_edge;  
-                Union(subsets, x, y);  
-            }  
-        }  
-      
-        printf("Minimum Spanning Tree:\n");  
-        for (i = 0; i < e; ++i) {  
-            printf("(%d, %d) -> %d\n", mst[i].src, mst[i].dest, mst[i].weight);  
-        }  
-    }  
-      
-    int main() {  
-        int V, E;  
-        printf("Enter number of vertices and edges: ");  
-        scanf("%d %d", &V, &E);  
-      
-        Graph* graph = createGraph(V, E);  
-      
-        printf("Enter edges and their weights:\n");  
-        for (int i = 0; i < E; ++i) {  
-            scanf("%d %d %d", &graph->edges[i].src, &graph->edges[i].dest, &graph->edges[i].weight);  
-        }  
-      
-        kruskalMST(graph);  
-      
-        return 0;  
-    }  
+#include <stdio.h>
+
+#define INF 999
+#define MAX 100
+
+int p[MAX], c[MAX][MAX], t[MAX][2];
+
+int find(int v) {
+    while (p[v]) {
+        v = p[v];
+    }
+    return v;
+}
+
+void union1(int i, int j) {
+    p[j] = i;
+}
+
+void kruskal(int n) {
+    int i, j, k, u, v, min, res1, res2, sum = 0;
+
+    for (k = 1; k < n; k++) {
+        min = INF;
+        res1 = res2 = -1;
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= n; j++) {
+                if (i == j) continue;
+                if (c[i][j] < min) {
+                    u = find(i);
+                    v = find(j);
+                    if (u != v) {
+                        res1 = i;
+                        res2 = j;
+                        min = c[i][j];
+                    }
+                }
+            }
+        }
+
+        if (res1 != -1 && res2 != -1) {
+            union1(find(res1), find(res2));
+            t[k][0] = res1;
+            t[k][1] = res2;
+            sum += min;
+        }
+    }
+
+    printf("\nCost of spanning tree is = %d", sum);
+    printf("\nEdges of spanning tree are:\n");
+    for (i = 1; i < n; i++) {
+        printf("%d -> %d\n", t[i][0], t[i][1]);
+    }
+}
+
+int main() {
+    int i, j, n;
+
+    printf("\nEnter the n value: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        p[i] = 0;
+    }
+
+    printf("\nEnter the graph data:\n");
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            scanf("%d", &c[i][j]);
+        }
+    }
+
+    kruskal(n);
+
+    return 0;
+}
