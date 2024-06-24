57411fa3a51187ff69db0f8227564d9c2e894352
3edsem <160483215+3edsem@users.noreply.github.com>
2024-06-24 20:03:59 +0530

    Update p6.c

--- p6.c
+++ p6.c
@@ -1,57 +1,46 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define MAX_VERTICES 100
-typedef struct {
-    int V;
-    int** adjMatrix;
-} Graph;
-
-
-Graph* createGraph(int V) {
-    Graph* graph = (Graph*)malloc(sizeof(Graph));
-    graph->V = V;
-    graph->adjMatrix = (int**)calloc(V, sizeof(int*));
-    for (int i = 0; i < V; i++) graph->adjMatrix[i] = (int*)calloc(V, sizeof(int));
-    return graph;
+int max(int a, int b) {
+    return (a > b) ? a : b;
 }
 
 
-void addEdge(Graph* graph, int src, int dest) {
-    graph->adjMatrix[src][dest] = 1;
-}
+int knapsack(int W, int wt[], int val[], int n) {
+    int i, w;
+    int K[n + 1][W + 1];
 
-
-void topologicalSort(Graph* graph) {
-    int V = graph->V, inDegree[MAX_VERTICES] = {0}, queue[MAX_VERTICES], front = 0, rear = -1;
-
-    for (int i = 0; i < V; i++)
-        for (int j = 0; j < V; j++)
-            if (graph->adjMatrix[i][j] == 1) inDegree[j]++;
-
-    for (int i = 0; i < V; i++) if (inDegree[i] == 0) queue[++rear] = i;
-
-    printf("Topological ordering of vertices: ");
-    while (front <= rear) {
-        int vertex = queue[front++];
-        printf("%d ", vertex);
-        for (int i = 0; i < V; i++) if (graph->adjMatrix[vertex][i] == 1 && --inDegree[i] == 0) queue[++rear] = i;
+   
+    for (i = 0; i <= n; i++) {
+        for (w = 0; w <= W; w++) {
+            if (i == 0 || w == 0)
+                K[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
+            else
+                K[i][w] = K[i - 1][w];
+        }
     }
-    printf("\n");
+
+   
+    return K[n][W];
 }
 
 int main() {
-    int V, E;
-    printf("Enter the number of vertices: ");
-    scanf("%d", &V);
-    Graph* graph = createGraph(V);
-    printf("Enter the number of edges: ");
-    scanf("%d", &E);
-    printf("Enter the edges (source vertex, destination vertex):\n");
-    for (int i = 0, src, dest; i < E; i++) {
-        scanf("%d %d", &src, &dest);
-        addEdge(graph, src, dest);
+    int val[100], wt[100]; 
+    int W, n; 
+    printf("Enter the number of items: ");
+    scanf("%d", &n);
+
+    printf("Enter the values and weights of %d items:\n", n);
+    for (int i = 0; i < n; i++) {
+        printf("Enter value and weight for item %d: ", i + 1);
+        scanf("%d %d", &val[i], &wt[i]);
     }
-    topologicalSort(graph);
+
+    printf("Enter the knapsack capacity: ");
+    scanf("%d", &W);
+
+    printf("Maximum value that can be obtained: %d\n", knapsack(W, wt, val, n));
+
     return 0;
 }
