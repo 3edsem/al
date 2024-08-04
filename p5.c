adaf7e55ca2c8a6bd2ab3b20d1717b229941a6ed
3edsem <160483215+3edsem@users.noreply.github.com>
2024-08-04 19:23:04 +0530

    Update p5.c

--- p5.c
+++ p5.c
@@ -1,57 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include<stdio.h>
+#include<stdlib.h>
 #define MAX_VERTICES 100
-typedef struct {
-    int V;
-    int** adjMatrix;
-} Graph;
-
-
-Graph* createGraph(int V) {
-    Graph* graph = (Graph*)malloc(sizeof(Graph));
-    graph->V = V;
-    graph->adjMatrix = (int**)calloc(V, sizeof(int*));
-    for (int i = 0; i < V; i++) graph->adjMatrix[i] = (int*)calloc(V, sizeof(int));
-    return graph;
+typedef struct{
+int V;
+int**adjMatrix;
+}Graph;
+Graph*createGraph(int V){
+Graph*graph=(Graph*)malloc(sizeof(Graph));
+graph->V=V;
+graph->adjMatrix=(int**)calloc(V,sizeof(int*));
+int i;
+for(i=0;i<V;i++)
+graph->adjMatrix[i]=(int*)calloc(V,sizeof(int));
+return graph;
 }
-
-
-void addEdge(Graph* graph, int src, int dest) {
-    graph->adjMatrix[src][dest] = 1;
+void addEdge(Graph*graph,int src,int dest){
+graph->adjMatrix[src][dest]=1;
 }
-
-
-void topologicalSort(Graph* graph) {
-    int V = graph->V, inDegree[MAX_VERTICES] = {0}, queue[MAX_VERTICES], front = 0, rear = -1;
-
-    for (int i = 0; i < V; i++)
-        for (int j = 0; j < V; j++)
-            if (graph->adjMatrix[i][j] == 1) inDegree[j]++;
-
-    for (int i = 0; i < V; i++) if (inDegree[i] == 0) queue[++rear] = i;
-
-    printf("Topological ordering of vertices: ");
-    while (front <= rear) {
-        int vertex = queue[front++];
-        printf("%d ", vertex);
-        for (int i = 0; i < V; i++) if (graph->adjMatrix[vertex][i] == 1 && --inDegree[i] == 0) queue[++rear] = i;
-    }
-    printf("\n");
+void topologicalSort(Graph*graph){
+int V=graph->V,inDegree[MAX_VERTICES]={0},queue[MAX_VERTICES],front=0,rear=-1;
+int i,j;
+for(i=0;i<V;i++)
+for(j=0;j<V;j++)
+if(graph->adjMatrix[i][j]==1)
+inDegree[j]++;
+for(i=0;i<V;i++)
+if(inDegree[i]==0)
+queue[++rear]=i;
+printf("Topological ordering of vertices:");
+while(front<=rear){
+int vertex=queue[front++];
+printf("%d",vertex);
+for(i=0;i<V;i++)
+if(graph->adjMatrix[vertex][i]==1&&--inDegree[i]==0)queue[++rear]=i;
 }
-
-int main() {
-    int V, E;
-    printf("Enter the number of vertices: ");
-    scanf("%d", &V);
-    Graph* graph = createGraph(V);
-    printf("Enter the number of edges: ");
-    scanf("%d", &E);
-    printf("Enter the edges (source vertex, destination vertex):\n");
-    for (int i = 0, src, dest; i < E; i++) {
-        scanf("%d %d", &src, &dest);
-        addEdge(graph, src, dest);
-    }
-    topologicalSort(graph);
-    return 0;
+printf("\n");
+}
+int main(int src,int dest){
+int V,E,i;
+printf("Enter the number of vertices:");
+scanf("%d",&V);
+Graph*graph=createGraph(V);
+printf("enter the number of edges:");
+scanf("%d",&E);
+printf("enter the edges(souce vertex,destination vaertex):\n");
+for(i=0,src,dest;i<E;i++){
+scanf("%d%d",&src,&dest);
+addEdge(graph,src,dest);
+}
+topologicalSort(graph);
+return 0;
 }
